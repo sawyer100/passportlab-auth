f84ab806d2708ba7dbabaf9a274832ee9937dd1b
sawyer100 <leonsawyer.dev@gmail.com>
2026-02-16 15:02:37 -0800

    red

--- routes/indexRoute.ts
+++ routes/indexRoute.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 import { ensureAuthenticated } from "../middleware/checkAuth";
 
 router.get("/", (req, res) => {
-  res.send("welcome");
+  res.redirect("/dashboard");
 });
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
--- something.d.ts
+++ something.d.ts
@@ -2,6 +2,7 @@ import "express";
 
 //idk if there is another type to use but i cant figure it out 
 //so im merging id into express.user because express.user isnt giving me a user.id 🤷‍♂️
+// im running with npx ts-node --project tsconfig.json --files app.ts
 declare global {
   namespace Express {
     interface User {
